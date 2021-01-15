6aa64f2f068fb0ffafac93eabeabf1f9bf9edbae
saturdaymakerkids <76859936+saturdaymakerkids@users.noreply.github.com>
2021-01-15 19:27:06 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+led.plot(1, 0)
+led.plot(3, 0)
+led.plot(2, 1)
+basic.forever(function () {
+    led.toggle(0, 2)
+    led.toggle(4, 2)
+    led.toggle(1, 3)
+    led.toggle(2, 3)
+    led.toggle(3, 3)
+    basic.pause(200)
+})
